9608e9361fb61a6ffff5325575f027a2648875c1
Tim Gert <tim.gert@in2code.de>
2025-11-17 12:03:25 +0100

    [TASK] Add multi site management and basic scss for styling with javascript and vite rendering

---
+++ timathedev.de/vite.config.mjs
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+// ESM-sicherer Ersatz für __dirname
+import { fileURLToPath } from 'url';
+const __dirname = resolve(fileURLToPath(import.meta.url), '..');
+
+export default defineConfig({
+    build: {
+        outDir: "dist",
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                index: resolve(__dirname, "index.html"),
+                about: resolve(__dirname, "about.html"),
+                projects: resolve(__dirname, "projects.html"),
+                arcade: resolve(__dirname, "arcade.html"),
+                contact: resolve(__dirname, "contact.html"),
+            },
+        },
+    },
+
+    // KEINE PLUGINS MEHR. FERTIG.
+    plugins: [],
+});
